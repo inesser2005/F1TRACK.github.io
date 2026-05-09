d19af86a3b2278f9a20416c2dc203984ebe85ff3
Inês Sérgio <inesser2005@gmail.com>
2026-05-09 23:18:12 +0100

    news.change

--- my-app/src/comp/F1NEWS/NewsSkeleton.jsx
+++ my-app/src/comp/F1NEWS/NewsSkeleton.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 
 const NewsSkeleton = () => (
-    <div className="f1-card skeleton-card">
-        <div className="f1-card-body">
-            {/* Simula a NewsImage */}
-            <div className="skeleton-image mb-3" style={{ height: "200px" }}></div>
-            <div className="mb-2">
-                {/* Simula o Badge */}
-                <div className="skeleton-badge mb-2" style={{ width: "80px", height: "15px" }}></div>
-                {/* Simula o Título */}
-                <div className="skeleton-title mb-1" style={{ width: "100%", height: "20px" }}></div>
-                <div className="skeleton-title" style={{ width: "60%", height: "20px" }}></div>
-            </div>
-            <div className="f1-card-footer">
-                {/* Simula a Descrição */}
-                <div className="skeleton-text mb-4" style={{ width: "100%", height: "40px", opacity: 0.5 }}></div>
-                {/* Simula o Botão */}
-                <div className="skeleton-btn" style={{ width: "100%", height: "38px" }}></div>
-            </div>
-        </div>
+  <div className="f1-card skeleton-card">
+    <div className="f1-card-body">
+      {/* Simula a NewsImage */}
+      <div className="skeleton-image mb-3" style={{ height: "200px" }}></div>
+      
+      <div className="mb-2">
+        {/* Simula o Badge "News Update" */}
+        <div className="skeleton-badge mb-2" style={{ width: "80px", height: "15px" }}></div>
+        
+        {/* Simula o Título (duas linhas) */}
+        <div className="skeleton-title mb-1" style={{ width: "100%", height: "20px" }}></div>
+        <div className="skeleton-title" style={{ width: "60%", height: "20px" }}></div>
+      </div>
+
+      <div className="f1-card-footer">
+        {/* Simula o Parágrafo de Descrição */}
+        <div className="skeleton-text mb-4" style={{ width: "100%", height: "40px", opacity: 0.5 }}></div>
+        
+        {/* Simula o Botão "Read the news!" */}
+        <div className="skeleton-btn" style={{ width: "100%", height: "38px" }}></div>
+      </div>
     </div>
+  </div>
 );
 
-export default NewsSkeleton; 
+export default NewsSkeleton;
