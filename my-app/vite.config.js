da0a65eab7cd6e3a6b15cff8866f7ab174287b5f
Inês Sérgio <inesser2005@gmail.com>
2026-05-09 19:12:01 +0100

    fix: final build config

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -2,15 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
-    }),
-  ],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
+  plugins: [react()],
 })
