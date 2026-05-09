83f47a85f1c261ae5037ff6e1e4997ef5602b081
Inês Sérgio <inesser2005@gmail.com>
2026-05-09 18:29:19 +0100

    lets hope for the best

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,27 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-
-export default defineConfig({
-  plugins: [react()],
-
-  // Define a pasta 'src' como o coração do projeto
-  root: 'src',
-
-  // Localização dos assets (relativo à pasta 'src')
-  publicDir: '../public',
-
-  build: {
-    // Onde os ficheiros finais serão colocados (relativo à pasta 'src')
-    outDir: '../dist',
-    // Limpa a pasta dist antes de cada build
-    emptyOutDir: true,
-    rollupOptions: {
-      // Como o 'root' é 'src', o caminho aqui deve ser direto para o ficheiro
-      input: resolve(__dirname, 'src/index.html'),
-    }
-  }
-})
 
 export default defineConfig({
   plugins: [
@@ -31,4 +9,9 @@ export default defineConfig({
       },
     }),
   ],
-});
+  // Removi o 'root: src' para o Vite encontrar o index.html na raiz da pasta my-app
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
+})
